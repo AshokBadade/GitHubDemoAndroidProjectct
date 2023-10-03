449b47b026c3c5b4e764335ab46153c5921426cb
AshokBadade <146696860+AshokBadade@users.noreply.github.com>
2023-10-04 00:43:33 +0530

    this is second commit to the code

--- app/src/main/java/com/ashok/githubdemo/MainActivity.kt
+++ app/src/main/java/com/ashok/githubdemo/MainActivity.kt
@@ -2,10 +2,17 @@ package com.ashok.githubdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getData() ;
+    }
+
+    fun getData(){
+        Toast.makeText(this,"this is second commit",Toast.LENGTH_LONG).show()
     }
 }
